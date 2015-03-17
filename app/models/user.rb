fd1f56b9d26b3852719f88be1fe9dc943f72b70a
GusVilla303 <gusvilla303@gmail.com>
2015-03-17 14:28:44 -0600

    Improve the wording in the user_spec file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true 
+  validates :email, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "validations" do
-    it "fail if user's email is empty" do
+  context "validation" do
+    it "is invalid if user's email is empty" do
       user = User.new(email: '')
 
       expect(user).to be_invalid
     end
 
-    it "fail if user's email is nil" do
+    it "is invalid if user's email is nil" do
       user = User.new(email: nil)
 
       expect(user).to be_invalid
     end
 
-    it "passes if user's email is valid" do
+    it "is valid if user has an email address" do
       user = User.new(email: 'customer@example.com')
 
       expect(user).to be_valid
     end
-
   end
-
-
 end
