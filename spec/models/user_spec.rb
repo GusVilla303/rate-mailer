c1a45f8dd2e6c317f6ce9ba12aa79edff424ee4a
GusVilla303 <gusvilla303@gmail.com>
2015-03-17 12:14:00 -0600

    Add User model and spec.  Tests for validating a user are passing (green).

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "fail if user's email is empty" do
+      user = User.new(email: '')
+
+      expect(user).to be_invalid
+    end
+
+    it "fail if user's email is nil" do
+      user = User.new(email: nil)
+
+      expect(user).to be_invalid
+    end
+
+    it "passes if user's email is valid" do
+      user = User.new(email: 'customer@example.com')
+
+      expect(user).to be_valid
+    end
+
+  end
+
+
 end
